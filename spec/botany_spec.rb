0a6825c416e9621b0fe00f469769a50cd0f55d10
John DeSilva <desilvjo@umich.edu>
2024-05-20 17:21:10 -0400

    Genesis commit

---
+++ spec/botany_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Botany do
+  it "has a version number" do
+    expect(Botany::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
