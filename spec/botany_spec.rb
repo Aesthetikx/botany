c240b40ca9cc6041349329e11a10e954ab284a53
John DeSilva <desilvjo@umich.edu>
2024-05-21 09:13:52 -0400

    Remove spec

--- spec/botany_spec.rb
+++ spec/botany_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Botany do
   it "has a version number" do
     expect(Botany::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
